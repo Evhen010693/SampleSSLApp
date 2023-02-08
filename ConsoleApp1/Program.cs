26a6f3f9ba6b76f2ddac0dfe7da11e2f5324b871
Yevhenii Sorokotiaha <e.sorokotiaga@creatio.com>
2023-02-08 17:24:46 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var handler = new HttpClientHandler
+{
+	ServerCertificateCustomValidationCallback = (request, cert, chain, errors) =>
+	{
+		Console.WriteLine("SSL error skipped");
+		return true;
+	}
+};
+HttpClient client = new HttpClient(handler);
+//NOT OK - https://exchange.sandbox.local/ews/exchange.asmx
+//OK - https://expired.badssl.com
+//OK - https://self-signed.badssl.com
+using HttpResponseMessage response = await client.GetAsync("https://exchange.sandbox.local/ews/exchange.asmx");
+
+//expect response statuscode 401
+string responseBody = await response.Content.ReadAsStringAsync();
+Console.WriteLine("Response body: " + responseBody);
